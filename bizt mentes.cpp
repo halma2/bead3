8bf53844bc92f57f04bff7d574177f7e7a487fc1
halma2 <105229129+halma2@users.noreply.github.com>
2022-05-09 20:05:23 +0200

    Add files via upload
    kiindulás main.cpp fájl

---
+++ bizt mentes.cpp
@@ -0,0 +1,74 @@
+#include "wingui.hpp"
+#include "Widget.hpp"
+#include "lista.hpp"
+#include "button.hpp"
+#include "spinbox.hpp"
+#include "inputText.hpp"
+#include <vector>
+#include <fstream>
+using namespace std;
+
+const int XX = 1080;
+const int YY = 480;
+
+struct Ablak : public wingui{
+	/*spinbox * s1;
+	spinbox * s2;
+
+	button * b1;
+	button * b2;
+	lista * l1;
+	lista * l2;
+	vector<string> * tomb1 =
+		new vector<string>({"elso","masodik","harmadik","negyedik","otodik","hatodik","hetedik"});
+	vector<string> * tomb2 = new vector<string>({"alma","korte","szilva"});*/
+
+    inputText * kijelzo;
+
+
+	Ablak(const int& xx, const int& yy):wingui(xx,yy) {
+       /* s1 = new spinbox(this,720,20,120,50,-15,5);
+        s2 = new spinbox(this,630,120,80,100,5,100);
+
+        b1 = new button(this, 300, 60, 30, 30, "->");
+        b1->lambd = [&](){d12pakol();};
+        b2 = new button(this, 300, 100, 30, 30, "<-");
+		b2->lambd = [&](){d21pakol();};
+
+        l1 = new lista(this,100,60,150,400,30,tomb1);
+        l2 = new lista(this,420,60,150,400,30,tomb2);*/
+
+        kijelzo = new inputText(this, 700,200,300,40);
+    }
+
+    void egy(){
+
+    }
+
+
+
+
+    /*void d12pakol(){
+    	if (l1->is_not_empty())
+			l2->paste(l1->cut());
+    }
+    void d21pakol(){
+    	if (l2->is_not_empty())
+			l1->paste(l2->cut());
+    }*/
+
+    virtual void kiiratas() override{
+		ofstream f("ertekek.txt");
+		/*f << "s1: " << s1->getValue() << "\ns2: " << s2->getValue() <<
+		"\nl1: " << l1->getValue() << "\nl2: " << l2->getValue();*/
+		f.close();
+    }
+};
+
+int main()
+{
+    Ablak a(XX, YY);
+    a.event_loop();
+
+    return 0;
+}
